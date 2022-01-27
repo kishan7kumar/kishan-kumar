c2e887bc64605bf6519c84871be8afa83ecb8894
unknown <kishankumar3202@gmail.com>
2022-01-27 23:57:52 +0530

    mobile layout first update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,19 +6,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 
+
+
 export class AppComponent {
   title = 'MyWebsite';
+  public getScreenWidth: any;
+  public getScreenHeight: any;
+  public isMobileDevice = false;
+  public showMenu = false;
 
-  redirect(website:any){
-    if (website==='linkedin'){
+  handleMenuBarClick() {
+    this.showMenu = !this.showMenu;
+  }
+
+  ngOnInit() {
+    // this.getScreenWidth = window.innerWidth;
+    // this.getScreenHeight = window.innerHeight;
+    // if (this.getScreenWidth < 1024) {
+    //   this.isMobileDevice = true;
+    // }
+    // if (this.getScreenWidth > 1024) {
+    //   this.isMobileDevice = false;
+    // }
+  }
+
+
+  // @HostListener('window:resize', ['$event'])
+  // onWindowResize() {
+  //   this.getScreenWidth = window.innerWidth;
+  //   this.getScreenHeight = window.innerHeight;
+  //   if (this.getScreenWidth < 1024) {
+  //     this.isMobileDevice = true;
+  //   }
+  //   if (this.getScreenWidth > 1024) {
+  //     this.isMobileDevice = false;
+  //   }
+  // }
+
+  redirect(website: any) {
+    if (website === 'linkedin') {
       window.location.href = "https://www.linkedin.com/in/kishankumar3202";
     }
-    if (website==='github'){
+    if (website === 'github') {
       window.location.href = "https://github.com/kishan7kumar";
     }
-    if(website==='hackerrank'){
+    if (website === 'hackerrank') {
       window.location.href = "https://www.hackerrank.com/kishankumar3202";
     }
-   
+
   }
 }
