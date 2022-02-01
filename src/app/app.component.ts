51ffb0c0b7d44f8435acd0784ad1e7edc80cc940
unknown <kishankumar3202@gmail.com>
2022-02-01 18:13:10 +0530

    download cv added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { PrimeNGConfig } from 'primeng/api';
-import { ViewportScroller } from "@angular/common";
+
 
 @Component({
   selector: 'app-root',
@@ -14,7 +14,7 @@ export class AppComponent {
   title = 'MyWebsite';
   public showMenu = false;
 
-  constructor(private router: Router, private primengConfig: PrimeNGConfig, private scroller: ViewportScroller) {
+  constructor(private router: Router, private primengConfig: PrimeNGConfig) {
   }
 
   ngOnInit() {
@@ -25,18 +25,28 @@ export class AppComponent {
     this.showMenu = !this.showMenu;
   }
 
+  scroll(el: HTMLElement) {
+    el.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  downloadCV() {
+    window.open(
+      "assets/Kishan_Kumar.pdf"
+    )
+  }
+
   handleTabClick() {
     if (window.matchMedia("only screen and (max-width: 1024px)").matches) {
       this.showMenu = !this.showMenu;
     }
   }
 
-  handlleSocialButtonClick(url:string){
+  handlleSocialButtonClick(url: string) {
     window.open(
       url,
       '_blank'
     )
   }
 
- 
+
 }
